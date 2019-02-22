30f44babb701c229e111a671c7927c936311dd84
Darren L <darren.lou@gmail.com>
2019-02-22 05:53:50 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -113,7 +113,7 @@ def changing_alan
      
      programmer_hash[:alan_kay][:known_for]
      
-    programmer_hash[:alan_kay][:known_for] = alans_new_info
+    programmer_hash[:alan_kay][:known_for] = "#{alans_new_info}"
      
 end
 
